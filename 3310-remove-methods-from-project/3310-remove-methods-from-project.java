f0081d71569ba54373202377a5c88f3369bdb147
Mohit Sharma <mohit9817304021@gmail.com>
2026-08-05 22:57:05 +0530

    Time: 46 ms (98.08%), Space: 281.6 MB (71.15%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : invocations) {
+            graph[edge[0]].add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(k);
+        suspicious[k] = true;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+
+            for (int next : graph[curr]) {
+                if (!suspicious[next]) {
+                    suspicious[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+
+        // If removal is impossible, return all methods
+        for (int[] edge : invocations) {
+            if (!suspicious[edge[0]] && suspicious[edge[1]]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        // Return remaining methods
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
